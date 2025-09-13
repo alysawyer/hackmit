5778663c17f25f314b59384126d60e9096eef083
Iris <irischen0505@gmail.com>
2025-09-13 13:30:52 -0400

    kinda works

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Character Create' }} />
+      <Stack.Screen name="index" options={{ title: 'Index' }} />
+      <Stack.Screen name="chrcreate" options={{ title: 'Character Create' }} />
     </Stack>
   );
 }
